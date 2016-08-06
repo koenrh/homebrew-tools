3bc4509580873cc38c52180bb9248a4186ebba14
Koen Rouwhorst <koenrh@users.noreply.github.com>
2016-08-06 14:29:57 +0200

    Add downloadrdslogs formula (#3)

---
+++ downloadrdslogs.rb
@@ -0,0 +1,10 @@
+class DownloadRDSLogs < Formula
+  desc 'Download all RDS logs for a given instance.'
+  homepage 'https://gist.github.com/koenrh/248f020468359fbfd15c2ef49a16bad5'
+  url 'https://gist.github.com/248f020468359fbfd15c2ef49a16bad5.git'
+  version '1' # gist revision number
+
+  def install
+    bin.install 'downloadrdslogs'
+  end
+end

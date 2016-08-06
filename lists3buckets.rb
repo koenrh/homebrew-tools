505e946ef430e53b13a7687ee426575abdc881f7
Koen Rouwhorst <info@koenrouwhorst.nl>
2016-08-06 15:01:39 +0200

    Add lists3buckets

---
+++ lists3buckets.rb
@@ -0,0 +1,10 @@
+class Lists3logs < Formula
+  desc 'List all S3 buckets, including their region.'
+  homepage 'https://gist.github.com/koenrh/0e23de58b176520add24'
+  url 'https://gist.github.com/0e23de58b176520add24.git'
+  version '3' # gist revision number
+
+  def install
+    bin.install 'lists3buckets'
+  end
+end

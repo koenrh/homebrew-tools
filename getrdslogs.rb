a18842045b30c2291a0282a58c0b10131693d6d9
Koen Rouwhorst <info@koenrouwhorst.nl>
2016-08-06 14:50:28 +0200

    Rename to getrdslogs

--- getrdslogs.rb
+++ getrdslogs.rb
@@ -1,10 +1,10 @@
-class Downloadrdslogs < Formula
+class Getrdslogs < Formula
   desc 'Download all RDS logs for a given instance.'
   homepage 'https://gist.github.com/koenrh/248f020468359fbfd15c2ef49a16bad5'
   url 'https://gist.github.com/248f020468359fbfd15c2ef49a16bad5.git'
-  version '3' # gist revision number
+  version '4' # gist revision number
 
   def install
-    bin.install 'downloadrdslogs'
+    bin.install 'getrdslogs'
   end
 end

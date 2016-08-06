4d7ed14572de60d20fa28e885ac8b0ef20624477
Koen Rouwhorst <info@koenrouwhorst.nl>
2016-08-06 14:13:02 +0200

    Bump diceware to v13

--- diceware.rb
+++ diceware.rb
@@ -4,7 +4,7 @@ class Diceware < Formula
   desc 'Generate strong pass phrases that you can remember.'
   homepage 'https://gist.github.com/koenrh/b3f21519f6e2ef82bc55'
   url 'https://gist.github.com/b3f21519f6e2ef82bc55.git'
-  version '12' # gist revision number
+  version '13' # gist revision number
 
   def install
     bin.install 'diceware'

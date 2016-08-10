c6517fd0ac922d876f1ae9125d95f514102b5586
Koen Rouwhorst <koenrh@users.noreply.github.com>
2016-08-10 14:12:41 +0200

    Bump to version 6 (#4)

--- getrdslogs.rb
+++ getrdslogs.rb
@@ -2,7 +2,7 @@ class Getrdslogs < Formula
   desc 'Download all RDS logs for a given instance.'
   homepage 'https://gist.github.com/koenrh/248f020468359fbfd15c2ef49a16bad5'
   url 'https://gist.github.com/248f020468359fbfd15c2ef49a16bad5.git'
-  version '4' # gist revision number
+  version '6' # gist revision number
 
   def install
     bin.install 'getrdslogs'

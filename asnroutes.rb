4b15e762c0ec1bea55dbe07ca357115553215887
Koen Rouwhorst <info@koenrouwhorst.nl>
2016-08-13 21:18:48 +0200

    Bump asnroutes to v3

--- asnroutes.rb
+++ asnroutes.rb
@@ -5,8 +5,8 @@ class Asnroutes < Formula
   desc 'Get a list of all announced prefixes for a given ASN.'
   homepage 'https://gist.github.com/koenrh/370ca1da644eba8ff8ac03f0233fb26d'
   url 'https://gist.github.com/370ca1da644eba8ff8ac03f0233fb26d.git'
-  version '2'
-  sha256 '72fe30e6a2e279f0f7ee0a6a76ed296853c80360641fb48e8f0aeb9220a5db37'
+  version '3'
+  sha256 '5cfb2eb8e4dc58b1ff1d3753eaf5059ab27a0c8368b5e0eaae1c681176bdbba0'
 
   def install
     bin.install 'asnroutes'

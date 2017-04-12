47dc9469d713c176b9ce679207afbd30852900e4
Koen Rouwhorst <info@koenrouwhorst.nl>
2017-04-12 21:22:20 +0200

    bump version archive-github-repository

--- archive-github-repository.rb
+++ archive-github-repository.rb
@@ -2,8 +2,8 @@ class ArchiveGithubRepository < Formula
   desc 'Archive public and private GitHub repositories, including issues, and wiki.'
   homepage 'https://github.com/koenrh/shell-scripts'
   url 'https://github.com/koenrh/shell-scripts.git'
-  version '0.1'
-  sha256 '0f7d8352f1ae71320bcb838f5ca415b6313bf86ba51abb4c5209e1edc68009ae'
+  version '0.2'
+  sha256 '562c70fcb454a29fbd2d9c04d88bf8e4f620ab8dd81384e07c99f3244052b9dc'
 
   def install
     bin.install 'archive-github-repository'

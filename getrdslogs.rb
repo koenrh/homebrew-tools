42deeda031761555d1e40c5e79843ba43120f4c9
Koen Rouwhorst <info@koenrouwhorst.nl>
2017-04-13 00:55:02 +0200

    reset version number

--- getrdslogs.rb
+++ getrdslogs.rb
@@ -2,7 +2,7 @@ class Getrdslogs < Formula
   desc 'Download all RDS logs for a given instance.'
   homepage 'https://github.com/koenrh/shell-scripts'
   url 'https://github.com/koenrh/shell-scripts.git'
-  version '6' # gist revision number
+  version '0.1'
   sha256 '55481e0c4a02b492101b54ad43e4365184b4ca1df266c739091f825e70ad82c2'
 
   def install

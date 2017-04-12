0ac1ea038741a7fb2793e8f07c6211431b5bd652
Koen Rouwhorst <info@koenrouwhorst.nl>
2017-04-13 00:44:38 +0200

    update asnroutes

--- asnroutes.rb
+++ asnroutes.rb
@@ -1,11 +1,11 @@
 class Asnroutes < Formula
   depends_on 'gawk'
-  depends_on 'homebrew/dupes/whois'
+  depends_on 'whois'
 
   desc 'Get a list of all announced prefixes for a given ASN.'
-  homepage 'https://gist.github.com/koenrh/370ca1da644eba8ff8ac03f0233fb26d'
-  url 'https://gist.github.com/370ca1da644eba8ff8ac03f0233fb26d.git'
-  version '3'
+  homepage 'https://github.com/koenrh/shell-scripts'
+  url 'https://github.com/koenrh/shell-scripts.git'
+  version '0.1'
   sha256 '5cfb2eb8e4dc58b1ff1d3753eaf5059ab27a0c8368b5e0eaae1c681176bdbba0'
 
   def install

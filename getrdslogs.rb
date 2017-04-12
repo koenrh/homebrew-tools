37d914e69da968c54a31f7b157755aa0f3fb7c52
Koen Rouwhorst <koenrh@users.noreply.github.com>
2017-04-13 00:56:21 +0200

    Update 'getrdslogs' formula (#13)
    * update formula
    
    * reset version number

--- getrdslogs.rb
+++ getrdslogs.rb
@@ -1,8 +1,9 @@
 class Getrdslogs < Formula
   desc 'Download all RDS logs for a given instance.'
-  homepage 'https://gist.github.com/koenrh/248f020468359fbfd15c2ef49a16bad5'
-  url 'https://gist.github.com/248f020468359fbfd15c2ef49a16bad5.git'
-  version '6' # gist revision number
+  homepage 'https://github.com/koenrh/shell-scripts'
+  url 'https://github.com/koenrh/shell-scripts.git'
+  version '0.1'
+  sha256 '55481e0c4a02b492101b54ad43e4365184b4ca1df266c739091f825e70ad82c2'
 
   def install
     bin.install 'getrdslogs'

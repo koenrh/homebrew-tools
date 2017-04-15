627cfcb0ca343387cf47e097b5a5a00318680afc
Koen Rouwhorst <koenrh@users.noreply.github.com>
2017-04-15 20:38:42 +0200

    Bump version to v0.1.1

--- github-bundle-audit.rb
+++ github-bundle-audit.rb
@@ -2,8 +2,8 @@ class GithubBundleAudit < Formula
   desc 'Audit Gemfiles in a single GitHub repository or all repositories in a GitHub organisation.'
   homepage 'https://github.com/koenrh/shell-scripts'
   url 'https://github.com/koenrh/shell-scripts.git'
-  version '0.1'
-  sha256 'ba001c0c380391c2d5b8713555d93588ac1d0388c52463a531c0da0182c5213f'
+  version '0.1.1'
+  sha256 'd9159ddb22ddca44560555252797f7c69cb77e5ecf3f5a1943223c17f41fd873'
 
   def install
     bin.install 'github-bundle-audit'

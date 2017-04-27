0e9b83a229759f7b8e0e830f7d880b45cb8bdedb
Koen Rouwhorst <info@koenrouwhorst.nl>
2017-04-27 18:15:42 +0200

    purl 0.1.1

--- purl.rb
+++ purl.rb
@@ -4,8 +4,8 @@ class Purl < Formula
   desc 'Get basic request and response timing information.'
   homepage 'https://github.com/koenrh/shell-scripts'
   url 'https://github.com/koenrh/shell-scripts.git'
-  version '0.1'
-  sha256 '7755a80f41a4178185c54a09dfb2964358b73c432b20353759e86f0f2613391c'
+  version '0.1.1'
+  sha256 'ae4653f863e1f1c8aa107169f83602e5f3467e0a8943371886a949773db28f6b'
 
   def install
     bin.install 'purl'

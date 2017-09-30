f896cd7a0786af2a784a065b1291a37fbd2baad6
Koen Rouwhorst <info@koenrouwhorst.nl>
2017-09-30 22:44:56 +0200

    Bump ungem.

--- ungem.rb
+++ ungem.rb
@@ -2,7 +2,7 @@ class Ungem < Formula
   desc 'Unpack Ruby a gem.'
   homepage 'https://github.com/koenrh/shell-scripts'
   url 'https://github.com/koenrh/shell-scripts.git'
-  version '0.1'
+  version '0.1.1'
 
   def install
     bin.install 'ungem'
